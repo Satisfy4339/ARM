698320c9db5af00472250e460b8ab18da06d362b
Satisfy4339 <satisfy.gamingserver@gmail.com>
2020-12-21 19:16:35 +0100

    About Command Edit (Added Server, CHannel and Member amount)

--- commands/about.js
+++ commands/about.js
@@ -1,5 +1,5 @@
 const { MessageEmbed } = require('discord.js');
-const { version, library, creator, botInvite, website, supportServer } = require('../config.json');
+const { version, library, creator, botInvite, supportServer } = require('../config.json');
 
 
 module.exports = {
@@ -13,6 +13,9 @@ module.exports = {
 	guildOnly: true,
 
 	execute(client, message) {
+		const servers = client.guilds.cache.size;
+		const channels = client.channels.cache.size;
+		const users = message.client.guilds.cache.map((g) => g.memberCount).reduce((a, c) => a + c);
 
 		let totalSeconds = (client.uptime / 1000);
 		const days = Math.floor(totalSeconds / 86400);
@@ -26,12 +29,14 @@ module.exports = {
 			.setColor('#70c7bc')
 			.setAuthor('ARM', client.user.displayAvatarURL({ format: 'png', dynamic: true }))
 			.addFields(
-				{ name: '**Version:**', value: version, inline: true },
-				{ name: '**Library:**', value: library, inline: true },
-				{ name: '**Creator:**', value: creator, inline: true },
-				{ name: '**Invite:**', value: botInvite, inline: true },
-				{ name: '**Website:**', value: website, inline: true },
-				{ name: '**Support Server:**', value: supportServer, inline: true },
+				{ name: '**Version**', value: version, inline: true },
+				{ name: '**Library**', value: library, inline: true },
+				{ name: '**Creator**', value: creator, inline: true },
+				{ name: '**Servers**', value: servers, inline: true },
+				{ name: '**Channels**', value: channels, inline: true },
+				{ name: '**Total Members**', value: users, inline: true },
+				{ name: '**Invite**', value: botInvite, inline: true },
+				{ name: '**Support Server**', value: supportServer, inline: true },
 			)
 			.setFooter(`Bot Uptime: ${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`);
 		message.channel.send(embed);
